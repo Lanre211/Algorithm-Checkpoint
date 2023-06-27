b678d1c21dc1959ea94628e284109941cb0bb1f0
Lanre <olawunmilanre00@gmail.com>
2023-06-27 16:39:52 +0100

    newalgo

---
+++ Checkpoint.js
@@ -0,0 +1,25 @@
+let sentence = "I am a Man.";
+let totalcharacter = sentence.length;
+console.log (totalcharacter)
+
+let wordNames = ["i", "am", "a", "Man"];
+console.log(wordNames.length);
+
+// list out all the vowels 
+// list the letters in the sentence
+// Big O is n
+// condition, if any of the vowel is same as letter count by 1
+// if vowel is not same as letter, dont count
+// let vowels be y
+// let sentence letters be n
+// loop althrough x 
+// while looping through each lettrs of n carry out loop while on each letters
+// COUNT +1 if variable is either "a", "e", "i", "o", "u" 
+
+function ()
+ //count vowels from sentence
+
+let n = ["I", "a", "m", "a", "M", "a", "n"]
+const y = ["a", "e", "i", "o", "u"];
+
+
